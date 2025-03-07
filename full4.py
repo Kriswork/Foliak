336e4ee3298e603a0a44c0fd9190796ae447efbe
Kriswork <99086681+Kriswork@users.noreply.github.com>
2025-03-07 12:46:07 +0100

    Update full4.py

--- full4.py
+++ full4.py
@@ -10,7 +10,7 @@ import adafruit_dht  # Biblioteka do obsługi DHT22
 # Ustawienia dla czujnika DHT22
 # DHT_SENSOR = Adafruit_DHT.DHT22
 # DHT_PIN = 4  # GPIO4 (Dostosuj do swojego podłączenia)
-dht_device = adafruit_dht.DHT22(board.D7)  # GPIO7
+dht_device = adafruit_dht.DHT22(board.D4)  # GPIO7
 
 min_value_asR = 22000
 max_value_asR = 65525
@@ -40,35 +40,34 @@ file_path = "nawodnienie_zapis.txt"
 # Tworzenie pliku, jeśli nie istnieje
 try:
     with open(file_path, "x") as file:
-        file.write("Data\tGodzina\tNapięcie(V)_C_(P0)\tRAW_C_(P0)\tProcent_C_(P0)\t"
-                   "Napięcie(V)_R_(P7)\tRAW_R_(P7)\tProcent_R_(P7)\tTemperatura(C)\tWilgotność(%)\n")
+        file.write("Data\tGodzina\tNapięcie(V)_C_(P0)\tRAW_C_(P0)\tProcent_C_(P0)\tNapięcie(V)_R_(P7)\tRAW_R_(P7)\tProcent_R_(P7)\t"
+                   "Napięcie(V)_C_(P1)\tRAW_C_(P1)\tProcent_C_(P1)\tTemperatura(C)\tWilgotność(%)\tLux raw\tLux V\n")
 except FileExistsError:
     pass
 
-time.sleep(1)
-
 while True:
     # Odczyt danych z MCP3008
-    chan7 = AnalogIn(mcp, MCP.P7)
-    chan0 = AnalogIn(mcp, MCP.P0)
+    chan7 = AnalogIn(mcp, MCP.P7) # sensor czujnik wilgotności gleby rezystancyjny
+    chan0 = AnalogIn(mcp, MCP.P0) # sensor czujnik wilgotności gleby pojemnościowy 1.2
+    chan1 = AnalogIn(mcp, MCP.P1) # sensor czujnik wilgotności gleby pojemnościowy 2.0
+    chan4 = AnalogIn(mcp, MCP.P4) # fotorezystor
     
-    ap0_voltage = chan0.voltage
-    ap0_raw = chan0.value
-    ap7_voltage = chan7.voltage
-    ap7_raw = chan7.value
+    as0_voltage = chan0.voltage
+    as0_raw = chan0.value
+    as1_voltage = chan1.voltage
+    as1_raw = chan1.value
+    as7_voltage = chan7.voltage
+    as7_raw = chan7.value 
     
     # Odczyt z DHT22
-    temperature = dht_device.temperature
-    humidity = dht_device.humidity
-    #humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    
-    if humidity is None or temperature is None:
-        humidity = "Błąd"
-        temperature = "Błąd"
-    else:
-        humidity = round(humidity, 2)
-        temperature = round(temperature, 2)
-    print("Temperatura: {temperature}, Wilgotność powietrze: {humidity}")
+    try:
+        temperature = dht_device.temperature
+        humidity = dht_device.humidity
+    except RuntimeError as error:
+        print(f"Błąd odczytu DHT22: {error}")
+        temperature = None
+        humidity = None
+
     # Pobranie aktualnej daty i godziny
     now = datetime.now()
     date_str = now.strftime("%Y-%m-%d")
@@ -77,11 +76,12 @@ while True:
     # Formatowanie danych do logowania
     log_entry = (f"{date_str}\t{time_str}\t{ap0_voltage:.2f}\t{ap0_raw}\t{sensor_to_percentage(ap0_raw, min_value_asC, max_value_asC)}%\t"
                  f"{ap7_voltage:.2f}\t{ap7_raw}\t{sensor_to_percentage(ap7_raw, min_value_asR, max_value_asR)}%\t"
-                 f"{temperature}\t{humidity}\n")
+                 f"{ap1_voltage:.2f}\t{ap1_raw}\t{sensor_to_percentage(ap1_raw, min_value_asR, max_value_asR)}%\t"
+                 f"{temperature}\t{humidity}\t{chan4.value}\t{chan4.voltage}\n")
     
     # Zapis do pliku
     with open(file_path, "a") as file:
         file.write(log_entry)
     
-    # print(log_entry.strip())
+    print(log_entry.strip())
     time.sleep(1800)
