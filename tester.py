2824fa9a1108cded909409e04ad55512082b4387
Kriswork <99086681+Kriswork@users.noreply.github.com>
2025-03-07 11:57:30 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -3,13 +3,18 @@ import digitalio
 import board
 import time
 import adafruit_mcp3xxx.mcp3008 as MCP
+import adafruit_dht  # Biblioteka do obsługi DHT22
 from adafruit_mcp3xxx.analog_in import AnalogIn
 from datetime import datetime
+
+dht_device = adafruit_dht.DHT22(board.D7)  # GPIO7 DHT22
 
 min_value_asR=22000
 max_value_asR=65525
 min_value_asC=18129
 max_value_asC=35000
+min_value_asC2=18129
+max_value_asC2=35000
 
 def sensor_to_percentage(value, min_value, max_value):
     """
@@ -39,23 +44,30 @@ print("cs D8: ", cs.value)
 mcp = MCP.MCP3008(spi, cs)
 
 # configure GPIO7 as analog input via MCP3008
-sensor = digitalio.DigitalInOut(board.D7)
+
 
 chan7 = AnalogIn(mcp, MCP.P7)
 chan0 = AnalogIn(mcp, MCP.P0)
+chan1 = AnalogIn(mcp, MCP.P1)
+chan4 = AnalogIn(mcp, MCP.P4)
 while True:
-    # create an analog input channel on pin: 0
 
+    
+    temperature = dht_device.temperature
+    humidity = dht_device.humidity
     
     # Read analog voltage and raw value from GPIO7
     as0_voltage = chan0.voltage
     as0_raw = chan0.value
+    as1_voltage = chan1.voltage
+    as1_raw = chan1.value
     as7_voltage = chan7.voltage
     as7_raw = chan7.value 
     print("")
-    print("cd D7: ", sensor.value)
+    print(f"Temperatura: {temperature}, Wilgotność powietrze: {humidity}, Wartości rezystora: {chan0.value} -> {chan0.voltage}")
     print(f"Wartość rezystancyjna  {as7_raw} -> {as7_voltage:.2f}V -> {sensor_to_percentage(as7_raw, min_value_asR, max_value_asR)}%")
     print(f"Wartość pojemnosciowa  {as0_raw} -> {as0_voltage:.2f}V -> {sensor_to_percentage(as0_raw, min_value_asC, max_value_asC)}%")
+    print(f"Wartość pojemnosciowa 2  {as1_raw} -> {as1_voltage:.2f}V -> {sensor_to_percentage(as1_raw, min_value_asC2, max_value_asC2)}%")
     print("")
     print("")
     
